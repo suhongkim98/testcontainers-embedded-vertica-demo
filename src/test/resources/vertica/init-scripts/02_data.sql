9ba05aa01c741f683129ea2168d74823467aa147
suhongkim98 <kshz9815@naver.com>
2023-01-27 15:43:43 +0900

    feature: 스크립트 파일을 정렬하여 순차적으로 적용한다.

--- src/test/resources/vertica/init-scripts/02_data.sql
+++ src/test/resources/vertica/init-scripts/02_data.sql
@@ -1,13 +1,6 @@
-/*----------------HELLO WORLD-----------------*/
-create table hello_world
-(
-    id INTEGER
-);
-
 START TRANSACTION;
 INSERT INTO hello_world(id) VALUES (1);
 INSERT INTO hello_world(id) VALUES (2);
 INSERT INTO hello_world(id) VALUES (3);
 INSERT INTO hello_world(id) VALUES (4);
 COMMIT;
-/*-----------------------------------*/
